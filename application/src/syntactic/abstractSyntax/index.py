29944be03953f2ac3f675279047a55fa992ce0d8
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-07-20 16:51:32 -0300

    feat: create all abstract class

---
+++ application/src/syntactic/abstractSyntax/index.py
@@ -0,0 +1,113 @@
+from abc import ABC
+
+
+class Init(ABC):
+    pass
+
+
+class Blockcode(ABC):
+    pass
+
+
+class Command(ABC):
+    pass
+
+
+class If(ABC):
+    pass
+
+
+class IfStatement(ABC):
+    pass
+
+
+class Elsif(ABC):
+    pass
+
+
+class Elsif2(ABC):
+    pass
+
+
+class Interations(ABC):
+    pass
+
+
+class For(ABC):
+    pass
+
+
+class ForAssignments(ABC):
+    pass
+
+
+class DoWhile(ABC):
+    pass
+
+
+class While(ABC):
+    pass
+
+
+class Function(ABC):
+    pass
+
+
+class FunctionAssignments(ABC):
+    pass
+
+
+class Call(ABC):
+    pass
+
+
+class Return(ABC):
+    pass
+
+
+class Exp(ABC):
+    pass
+
+
+class ExpLor(ABC):
+    pass
+
+
+class ExpLand(ABC):
+    pass
+
+
+class ExpOr(ABC):
+    pass
+
+
+class ExpAnd(ABC):
+    pass
+
+
+class ExpCompEq(ABC):
+    pass
+
+
+class ExpComp(ABC):
+    pass
+
+
+class ExpPlusMinus(ABC):
+    pass
+
+
+class ExpTimesDivides(ABC):
+    pass
+
+
+class ExpLnot(ABC):
+    pass
+
+
+class ExpDecrementIncrement(ABC):
+    pass
+
+
+class ExpLastLayer(ABC):
+    pass
